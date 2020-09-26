aa983a8ed7f7f17433d24593dac2c75573b38b72
Lisandro Scarrone <lisandro.sebsca@gmail.com>
2020-09-26 17:46:20 -0300

    Add cards
    Create new cards displaying id, name, types and an image.

--- src/main.js
+++ src/main.js
@@ -8,61 +8,72 @@ function getPokemon(pokemonID) {
     });
 }
 
-[...Array(9).keys()].forEach((key) => {
-    pokemonID = key + 1;
-    getPokemon(pokemonID);
-
-    // showCard();
-    // getImage(pokemonID);
-});
-
 function createCard(pokemon) {
     getInfo(pokemon);
 }
 
 function getInfo(pokemon) {
-    const ID = pokemon['id'];
+    const id = pokemon['id'];
     const name = pokemon['species']['name'];
-    const height = pokemon['height'];
-    const weight = pokemon['weight'];
-    const type0 = pokemon['types']['0']['type']['name'];
-    let type1 = '';
+    // const height = pokemon['height'];
+    // const weight = pokemon['weight'];
+    const type1 = pokemon['types']['0']['type']['name'];
+    let type2 = '';
     if (pokemon['types']['1']) {
-        type1 = pokemon['types']['1']['type']['name'];
+        type2 = pokemon['types']['1']['type']['name'];
     }
-    const $container = $('.container');
-    $container.append(
-        `<div class='pokemon' id='${name}' > ${name} - #${ID} </br> ${type0} ${type1} </br> Height: ${height} </br> Weight: ${weight} </div>`
-    );
-    // getDescription(pokemon);
-    // getType(pokemon);
-    // getHeight(pokemon);
-    // getWeight(pokemon);
-    // getAbilities(pokemon);
-    // getStats(pokemon);
+    let imageID = '';
+    if (id < 10) {
+        imageID = `00${id}`;
+    } else if (id < 100) {
+        imageID = `0${id}`;
+    } else {
+        imageID = id;
+    }
+
+    const card = `
+        <div class="card" id="${id}">
+            <img
+            src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${imageID}.png"
+            alt=""
+            style="width: 200px; height: 200px"
+            />
+
+            <div class="card-stats">
+                <div class="types">
+                    <div class="type ${type1}">${type1.toUpperCase()}</div>
+                    <div class="type ${type2}"  id="${id}-type" >${type2.toUpperCase()}</div>
+                </div>
+            
+                <div class="name">
+                    <br>#${id}</br>${
+        name.charAt(0).toUpperCase() + name.slice(1)
+    }</h3>
+                    
+                </div>
+            </div>
+        </div>`;
+    const cardObject = {
+        cardID: id,
+        cardHTML: card,
+    };
+    showCard(cardObject);
+    hideType2(id, type2);
+    return cardObject;
 }
 
-arceus = {
-    id: 493,
-    species: {
-        name: 'arceus',
-    },
-    stats: {
-        0: {
-            stat: {
-                name: 'hp',
-            },
-            base_stat: 120,
-        },
-    },
-    height: 32,
-    weight: 3200,
-    types: {
-        0: {
-            slot: 1,
-            type: {
-                name: 'normal',
-            },
-        },
-    },
-};
+function showCard(cardObject) {
+    const $container = $('.container');
+    $container.append(cardObject['cardHTML']);
+}
+
+function hideType2(id, type2) {
+    if (!type2) {
+        $(`#${id}-type`).addClass('hidden');
+    }
+}
+
+[...Array(9).keys()].forEach((key) => {
+    pokemonID = key + 1;
+    getPokemon(pokemonID);
+});
